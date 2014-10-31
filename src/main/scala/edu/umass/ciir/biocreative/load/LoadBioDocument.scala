83265f2b2dccd3980084de9baaa1cec91f0d2c49
Laura Dietz <dietz@cs.umass.edu>
2014-10-31 15:43:01 -0400

    generating entrez-diskbacker files

--- src/main/scala/edu/umass/ciir/biocreative/load/LoadBioDocument.scala
+++ src/main/scala/edu/umass/ciir/biocreative/load/LoadBioDocument.scala
@@ -4,6 +4,7 @@ import java.io._
 import java.util.zip.GZIPInputStream
 
 import edu.umass.ciir.biocreative.{BioNames, Name}
+import edu.umass.ciir.strepsi.MainTools
 import edu.umass.ciir.strepsimur.DiskBacking
 
 import scala.collection.mutable.ListBuffer
@@ -51,5 +52,11 @@ object LoadBioDocument {
       val nameToBioNames = loadMap(new java.io.File(inputFile))
       val stringstringMap = nameToBioNames.map(x => x._1 -> x._2.toString)
       DiskBacking.createStringStringDiskBackingImm(stringstringMap, outputFile)
+  }
+
+  def main(args:Array[String]): Unit ={
+    val inputFile =MainTools.strsFromArgsSimple(args, "-inputFile=",1,cutPrefix = true).head
+    val outputFile =MainTools.strsFromArgsSimple(args, "-outputFile=",1,cutPrefix = true).head
+    convertToDiskBacker(inputFile,outputFile)
   }
 }
